3aa402dbfd4ec6e5a2ceec45b402397e4b6531b0
Alexandr Butuzov <sasha.butuzov99@mail.ru>
2022-05-10 04:48:43 +0500

    delete unnecessary

--- Direct.Client/Providers/AuthTokenProvider.cs
+++ Direct.Client/Providers/AuthTokenProvider.cs
@@ -5,7 +5,6 @@ namespace Direct.Client.Providers
     public class AuthTokenProvider : IAuthTokenProvider
     {
         public string GetToken() {
-            //var token = await client.GetAsync("https://oauth.yandex.ru/authorize?response_type=token&client_id="+appID);
             return "AQAAAABgnkudAAfhe1NaPuIsDEWOg1-x0Xawiro";
         }
     }
