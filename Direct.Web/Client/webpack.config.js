b3722dfcc70c1f7f180f77674cf43d7369b975d1
Alexandr Butuzov <sasha.butuzon99@mail.ru>
2022-06-05 02:47:22 +0500

    add test fetch

--- Direct.Web/Client/webpack.config.js
+++ Direct.Web/Client/webpack.config.js
@@ -3,18 +3,30 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: "./app.jsx", // входная точка - исходный файл
+    devtool: 'source-map',
     output:{
         path: path.resolve(__dirname, "./dist"),     // путь к каталогу выходных файлов - папка public
         publicPath: "/dist/",
         filename: "bundle.js"       // название создаваемого файла
     },
+
     devServer: {
      historyApiFallback: true,
      static: {
       directory: path.join(__dirname, "/"),
     },
      port: 8081,
-     open: true
+     open: true,
+     headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    },
+     proxy: {
+         '/api': {
+             target: 'http://192.168.1.243:90'
+        }
+    }
    },
     module:{
         rules:[   //загрузчик для jsx
