e575da24673d1301bd6912308d9f3055d200a337
Alexandr Butuzov <sasha.butuzon99@mail.ru>
2022-06-13 16:05:03 +0500

    Change AddCheck

--- Direct.Web/Client/components/header.jsx
+++ Direct.Web/Client/components/header.jsx
@@ -7,11 +7,6 @@ class Header extends React.Component {
             <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
                 <div className="container">
                     <img className="navbar-brand" width="250" src='static/images/headerImage.png' alt="logo"/>
-                    {/* <a className="navbar-brand" asp-area="" asp-controller="Home" asp-action="Index">Direct.Web.React</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>                */}
                 </div>
             </nav>
         </header>
@@ -20,3 +15,5 @@ class Header extends React.Component {
 }
   
 export default Header;
+
+
